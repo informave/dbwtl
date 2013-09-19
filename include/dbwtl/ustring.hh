57430ca0f4b46a91d3ed60e0b49928a4200d9ee2
Daniel Vogelbacher <daniel@vogelbacher.name>
2013-09-19 19:01:51 +0200

    LIKELY() and UNLIKELY() macros only on GCC

--- include/dbwtl/ustring.hh
+++ include/dbwtl/ustring.hh
@@ -61,10 +61,13 @@
 
 DB_NAMESPACE_BEGIN
 
-
+#ifdef __GNUC__
 #define LIKELY(x)      __builtin_expect(!!(x), 1)
 #define UNLIKELY(x)    __builtin_expect(!!(x), 0)
-
+#else
+#define LIKELY(x)      (x)
+#define UNLIKELY(x)    (x)
+#endif
 
 typedef uint16_t UCharT;
 

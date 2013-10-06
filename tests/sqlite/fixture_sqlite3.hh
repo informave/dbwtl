e3b22d47ff529246cd2e7e42547e835447db8de3
Daniel Vogelbacher <daniel@vogelbacher.name>
2013-10-06 17:18:55 +0200

    add firebird and sqlite tests

---
+++ tests/sqlite/fixture_sqlite3.hh
@@ -0,0 +1,44 @@
+#include <dbwtl/dbobjects>
+#include <dbwtl/dal/engines/sqlite>
+#include <dbwtl/dal/engines/generic>
+#include <dbwtl/ustring>
+
+
+
+#include "../cxxc.hh"
+
+
+
+
+using namespace informave::db;
+typedef Database<sqlite> DBMS;
+
+
+struct SqliteMemoryFixture
+{
+    SqliteMemoryFixture(void)
+        : env("sqlite:libsqlite"),
+          dbc(env)
+    {}
+
+    virtual ~SqliteMemoryFixture(void)
+    {}
+    
+    void onSetUp(void)
+    {
+        dbc.connect(L":memory:");
+    }
+    
+    DBMS::Environment env;
+    DBMS::Connection dbc;
+};
+
+
+//
+// Local Variables:
+// mode: C++
+// c-file-style: "bsd"
+// c-basic-offset: 4
+// indent-tabs-mode: nil
+// End:
+//

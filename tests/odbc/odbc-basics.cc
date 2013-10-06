85affd01838e1fb880bdd9d34852754846900205
Daniel Vogelbacher <daniel@vogelbacher.name>
2013-10-06 21:40:22 +0200

    updated for travis ci tests #46

---
+++ tests/odbc/odbc-basics.cc
@@ -0,0 +1,52 @@
+
+#include <fstream>
+
+#include <dbwtl/dal/engines/generic>
+#include "../cxxc.hh"
+#include "fixture_odbc.hh"
+
+
+
+CXXC_FIXTURE_TEST(OdbcPgFixture, Transaction)
+{
+        DBMS::Statement stmt(dbc);
+        dbc.beginTrans(trx_read_committed);
+	dbc.rollback();
+	dbc.rollback();
+	dbc.beginTrans(trx_read_committed);
+	dbc.beginTrans(trx_read_committed);
+
+}
+
+
+CXXC_FIXTURE_TEST(OdbcPgFixture, MetaTables)
+{
+	DBMS::Metadata md(dbc);
+
+	RecordSet rs = md.getTables(Variant(), Variant(), Variant(), IMetadata::META_OBJECT_CLASS_ALL);
+	rs.first();
+	while( !rs.eof() )
+	{
+		for(colnum_t i = 1; i <= rs.columnCount(); ++i)
+		{
+			std::cout << ifnull<String>(rs.column(i), "<null>") << "\t|\t";
+		}
+		rs.next();
+		std::cout << std::endl;
+	}
+}
+
+
+int main(void)
+{
+    std::locale::global(std::locale(""));
+    std::cout.imbue(std::locale());
+    std::cerr.imbue(std::locale());
+    std::clog.imbue(std::locale());
+    std::wcout.imbue(std::locale());
+    std::wcerr.imbue(std::locale());
+    std::wclog.imbue(std::locale());
+
+    return cxxc::runAll();
+}
+

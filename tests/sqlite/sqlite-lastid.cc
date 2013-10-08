a1d0390958dd0b54a4c43a094cfd22c9e59496aa
Daniel Vogelbacher <daniel@vogelbacher.name>
2013-10-08 22:19:07 +0200

    updated for travis ci tests #62

---
+++ tests/sqlite/sqlite-lastid.cc
@@ -0,0 +1,46 @@
+#include <dbwtl/dal/dalinterface>
+#include <dbwtl/dal/engines/sqlite>
+#include <dbwtl/dal/engines/generic>
+#include <dbwtl/dbobjects>
+#include <dbwtl/ustring>
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <cstdlib>
+
+#include "../cxxc.hh"
+#include "fixture_sqlite3.hh"
+
+
+CXXC_FIXTURE_TEST(SqliteMemoryFixture, DoubleResultset)
+{
+    dbc.directCmd("CREATE TABLE test(id INTEGER PRIMARY KEY, data TEXT);");
+    
+    DBMS::Statement stmt(dbc);
+    stmt.prepare("INSERT INTO test VALUES(?, ?)");
+    stmt.bind(1, 1234);
+    stmt.bind(2, String("testdata"));
+    stmt.execute();
+
+    DBMS::Variant id = stmt.lastInsertRowId();
+    
+    stmt.close();
+
+    CXXC_CHECK( id.asBigint() == 1234 );
+}
+
+
+int main(void)
+{
+    std::locale::global(std::locale(""));
+    std::cout.imbue(std::locale());
+    std::cerr.imbue(std::locale());
+    std::clog.imbue(std::locale());
+    std::wcout.imbue(std::locale());
+    std::wcerr.imbue(std::locale());
+    std::wclog.imbue(std::locale());
+
+    return cxxc::runAll();
+}
+
